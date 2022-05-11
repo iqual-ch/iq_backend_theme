fce915902d272be55c1d1ac981062b626c9b9717
Daniel Wirz <d.wirz@iqual.ch>
2022-05-11 13:10:01 +0200

    frontend adjustments

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+
+ Drupal.webform = Drupal.webform || {};
+ Drupal.webform.htmlEditor = Drupal.webform.htmlEditor || {};
+ Drupal.webform.htmlEditor.options = Drupal.webform.htmlEditor.options || {};
+ Drupal.webform.htmlEditor.options.contentsCss  = ['/themes/custom/iq_backend_theme/resources/css/gin-ckeditor.css']
+
